9dc249e9f339f32466e33a21998dc5cfcb45ad69
Shahmir Ejaz <shaejaz5@gmail.com>
2022-11-29 21:19:32 +0000

    update dependencies to artigen-lib

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,60 +1,26 @@
 extern crate jni;
 
-use std::ffi::CString;
-use std::{io::{Cursor}, vec};
-use std::os::raw::c_char;
-use rand::Rng;
-
-use imageproc::integral_image::ArrayData;
-use base64::{encode};
+use artigen::patterns::{julia, Pattern};
+use artigen::output::base64;
 
 use jni::JNIEnv;
-use jni::objects::{JClass, JList, JObject, JValue};
-use jni::sys::{jint, jintArray, jlong, jobject, jstring};
-use jni::signature::{ReturnType, Primitive};
+use jni::objects::{JClass};
+use jni::sys::{jstring};
 
 fn generate_image() -> String {
     let imgx = 1080;
     let imgy = 2213;
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 6.67 / imgy as f32;
-
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let a = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([a, 0, b]);
-    }
-
-    // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
-
-            let c = num_complex::Complex::new(-0.4, 0.6);
-            let mut z = num_complex::Complex::new(cx, cy);
-
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z + c;
-                i += 1;
-            }
-
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+    let julia_pat = julia::Julia {
+        config: julia::JuliaConfig {
+            x: imgx,
+            y: imgy
         }
-    }
+    };
 
-    let mut buf = Cursor::new(vec![]);
-    imgbuf.write_to(&mut buf, image::ImageOutputFormat::Png).expect("TEST");
+    let img = julia_pat.generate();
 
-    encode(buf.into_inner())
+    base64::generate_from_image(img)
 }
 
 #[no_mangle]
